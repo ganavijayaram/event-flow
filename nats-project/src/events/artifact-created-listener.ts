f3a239bd3c27ff27b5900239a6acc02c1de509ff
ganavijayaram <jayaram.g@northeastern.edu>
2023-08-26 12:01:29 -0400

    Added enum

--- nats-project/src/events/artifact-created-listener.ts
+++ nats-project/src/events/artifact-created-listener.ts
@@ -1,8 +1,9 @@
 import { Message } from 'node-nats-streaming'
 import {Listener} from './base-listener'
+import { Subjects } from './subjects'
 
 export class ArtifactCreatedListener extends Listener {
-  subject = 'artifact:created'
+  subject = Subjects.ArtifactCreated
   queueGroupName = 'payments-service'
   onMessage(data: any, msg: Message): void {
     console.log('Event data! ', data)
---
+++ nats-project/src/events/subjects.ts
@@ -0,0 +1,6 @@
+// this is the list of all the channels
+// whenever we say the value is type Subjects, it has to have one of teh values which are defined inside it
+export enum Subjects {
+  ArtifactCreated =  'artifact:created',
+  OrderUpdated = 'order:updated'
+} 
